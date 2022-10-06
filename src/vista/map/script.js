ecba1f5271f2db4ed4510a94495a7562970fc16f
Gonzalo <gvaldez-99@hotmail.com>
2022-10-06 21:04:16 +0200

    added function DrawMarker

--- src/vista/map/script.js
+++ src/vista/map/script.js
@@ -1,33 +1,4 @@
 
-let museoDelPrado = [40.41395, 0 - 3.69215]
-let museoThyssen = [40.41618, -3.69490];
-let laCaixaForum = [40.41122, -3.69358];
-let museoReinaSofia = [40.40805, -3.69460];
-let teatroReal = [40.41847, -3.71055];
-let jardinesSabatini = [40.42048, -3.71405];
-let palacioReal = [40.41803, -3.71438];
-let catedralAlmudena = [40.41596, -3.71455];
-//let madrid = [40.4165, -3.70256];
-let coordRetiro = [40.41317, -3.68307];
-let puertaDeAlcala = [40.42028, -3.68880];
-let plazaMayor = [40.41583, -3.70738];
-let museoSorolla = [40.43548, -3.69247];
-let berna = [40.45316, -3.68832];
-let torres = [40.47729, -3.68798];
-let wanda = [40.43633, -3.59948];
-let warner = [40.44566, -3.65693];
-let abc = [40.43256, -3.68720];
-let parqueAtraccion = [40.41203, -3.75009];
-let wizink = [40.42400, -3.67173];
-let palacioVista = [40.38614, -3.738];
-let ojala = [40.42556, -3.70394];
-let cherry = [40.44120, -3.71532];
-let oven = [40.42526, -3.70073];
-let steakBurger = [40.41990, -3.69937];
-let bancoEspaña = [40.41872, -3.69443];
-let farmacia = [40.41966, -3.67130];
-let supermercado = [40.41793, -3.67321];
-
 var Icono = L.icon({
   iconUrl: "https://vivaelsoftwarelibre.com/wp-content/uploads/2020/05/icono.png",
   iconSize: [30, 40],
@@ -177,231 +148,57 @@ var IconoParqueAtracciones = L.icon({
   shadowAnchor: [0, 55],
   popupAnchor: [0, -40]
 });
+//let madrid = [40.4165, -3.70256];
+let locationData = [{Coordenadas: [40.41395, -3.69215], Nombre : "Museo del Prado", Icono : IconoMonumento},
+{Coordenadas : [40.41618, -3.69490], Nombre : "Museo Thyssen", Icono: IconoMonumento},
+{Coordenadas : [40.41122, -3.69358], Nombre : "Caixa Forum", Icono: IconoMonumento},
+{Coordenadas : [40.40805, -3.69460], Nombre : "Museo Reina Sofia", Icono: IconoMonumento},
+{Coordenadas : [40.41847, -3.71055], Nombre : "Teatro Real", Icono: IconoCine},
+{Coordenadas : [40.42048, -3.71405], Nombre : "Jardines Sabatini", Icono: IconoVerde},
+{Coordenadas : [40.41803, -3.71438], Nombre : "Palacio Real", Icono: IconoMonumento},
+{Coordenadas : [40.41596, -3.71455], Nombre : "Catedral Almudena", Icono: IconoIglesia},
+{Coordenadas: [40.41317, -3.68307], Nombre: "Parque del Retiro", Icono: IconoVerde},
+{Coordenadas: [40.42028, -3.68880], Nombre: "Puerta de Alcala", Icono: IconoMonumento},
+{Coordenadas: [40.41583, -3.70738], Nombre: "Plaza Mayor", Icono: IconoMonumento},
+{Coordenadas: [40.43548, -3.69247], Nombre: "Museo Sorolla", Icono: IconoMonumento},
+{Coordenadas: [40.45316, -3.68832], Nombre: "Estadio Santiago Bernabeu", Icono: IconoDeportes},
+{Coordenadas: [40.47729, -3.68798], Nombre: "Las Cuatro Torres", Icono: IconoEmpresa},
+{Coordenadas: [40.43633, -3.59948], Nombre: "Estadio Civitas Metropolitano", Icono: IconoDeportes},
+{Coordenadas: [40.44566, -3.65693], Nombre: "Parque de atracciones Warner", Icono: IconoParqueAtracciones},
+{Coordenadas: [40.43256, -3.68720], Nombre: "Centro comercial ABC Serrano", Icono: IconoWifi},
+{Coordenadas: [40.41203, -3.75009], Nombre: "Parque de Atracciones de Madrid", Icono: IconoParqueAtracciones},
+{Coordenadas: [40.42400, -3.67173], Nombre: "Wizink Center", Icono: IconoMusica},
+{Coordenadas: [40.38614, -3.738], Nombre: "Palacio Vistalegre Arena", Icono: IconoMusica},
+{Coordenadas: [40.42556, -3.70394], Nombre: "Ojala Bar", Icono: IconoRestaurantes},
+{Coordenadas: [40.44120, -3.71532], Nombre: "Cherry Pecas Bar", Icono: IconoRestaurantes},
+{Coordenadas: [40.42526, -3.70073], Nombre: "Oven Restaurante", Icono: IconoComida},
+{Coordenadas: [40.41990, -3.69937], Nombre: "Steak Burger Restaurante", Icono: IconoComida},
+{Coordenadas: [40.41872, -3.69443], Nombre: "Banco de España", Icono: IconoBancos},
+{Coordenadas: [40.41966, -3.67130], Nombre: "Hospital Gregorio Marañon", Icono: IconoFarmacias},
+{Coordenadas: [40.41793, -3.67321], Nombre: "Carrefour", Icono: IconoSuper},
+{Coordenadas: [40.39801, -3.83492], Nombre: "Universidad CEU San Pablo Monteprincipe", Icono: IconoMonumento},
+]
 
 
-
-var map = L.map('map').setView([40.41317, -3.68307], 15);
+var map = L.map('map').setView([40.39801, -3.83492], 15);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18
 }).addTo(map);
 
 L.control.scale().addTo(map);
-L.marker([40.39801, -3.83492], { draggable: false }).addTo(map);
 
+function DrawMarkers(data){
+    L.marker(data.Coordenadas, {
+      title: data.Nombre,
+      draggable: false,
+      opacity: 0.95,
+      icon: data.Icono
+    }).bindPopup("<i>" + data.Nombre + "</i>")
+      .addTo(map);
+}
 
-var MuseoPrado = L.marker(museoDelPrado, {
-  title: "Museo del prado",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Museo del Prado</i>")
-  .addTo(map);
+for(var key in locationData){
+  DrawMarkers(locationData[key])
+}
 
-var Thyssen = L.marker(museoThyssen, {
-  title: "Museo Thyssen",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Museo Thyssen</i>")
-  .addTo(map);
-
-var Caixa = L.marker(laCaixaForum, {
-  title: "Caixa Forum",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Caixa Forum</i>")
-  .addTo(map);
-
-var ReinaSfia = L.marker(museoReinaSofia, {
-  title: "Museo Reina Sofia",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Museo Reina Sofia</i>")
-  .addTo(map);
-
-var TeatroRl = L.marker(teatroReal, {
-  title: "Teatro Real",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoCine
-}).bindPopup("<i>Teatro Real</i>")
-  .addTo(map);
-
-var JardinesSab = L.marker(jardinesSabatini, {
-  title: "Jardines Sabatini",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoVerde
-}).bindPopup("<i>Jardines Sabatini</i>")
-  .addTo(map);
-
-var PalacioR = L.marker(palacioReal, {
-  title: "Palacio Real",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Palacio Real</i>")
-  .addTo(map);
-
-var Almudena = L.marker(catedralAlmudena, {
-  title: "Catedral de la Almudena",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoIglesia
-}).bindPopup("<i>Catedral de la Almudena</i>")
-  .addTo(map);
-
-var Retiro = L.marker(coordRetiro, {
-  title: "Retiro",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoVerde
-}).bindPopup("<i>Retiro</i>")
-  .addTo(map);
-
-var PuertaAlcala = L.marker(puertaDeAlcala, {
-  title: "Puerta de Alcala",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Puerta de Alcala</i>")
-  .addTo(map);
-
-var PlazaMay = L.marker(plazaMayor, {
-  title: "Plaza Mayor",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Plaza Mayor</i>")
-  .addTo(map);
-
-var Sorolla = L.marker(museoSorolla, {
-  title: "Museo Sorolla",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMonumento
-}).bindPopup("<i>Museo Sorolla</i>")
-  .addTo(map);
-
-var Bernabeu = L.marker(berna, {
-  title: "Estadio Santiago Bernabeu",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoDeportes
-}).bindPopup("<i>Estadio Santiago Bernabeu</i>")
-  .addTo(map);
-
-var CuatroTorres = L.marker(torres, {
-  title: "Las Cuatro Torres",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoEmpresa
-}).bindPopup("<i>Las Cuatro Torres</i>")
-  .addTo(map);
-
-var metropolitano = L.marker(wanda, {
-  title: "Estadio Civitas Metropolitano",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoDeportes
-}).bindPopup("<i>Estadio Civitas Metropolitano</i>")
-  .addTo(map);
-
-var ParqueWar = L.marker(warner, {
-  title: "Parque Warner",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoParqueAtracciones
-}).bindPopup("<p>Parque Warner</p>")
-  .addTo(map);
-
-var abcSerrano = L.marker(abc, {
-  title: "Abc Serrano",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoWifi
-}).bindPopup("<p>Centro Comercial ABC Serrano</p>")
-  .addTo(map);
-
-var elParqueDeAtracciones = L.marker(parqueAtraccion, {
-  title: "Parque de Atracciones",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoParqueAtracciones
-}).bindPopup("<p>Parque de Atracciones</p>")
-  .addTo(map);
-
-var wizinkCenter = L.marker(wizink, {
-  title: "Wizink Center",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMusica
-}).bindPopup("<p>Wizink Center</p>")
-  .addTo(map);
-
-var palacioVistalegre = L.marker(palacioVista, {
-  title: "Palacio Vistalegre Arena",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoMusica
-}).bindPopup("<p>Palacio Vistalegre Arena</p>")
-  .addTo(map);
-
-var oajalaBar = L.marker(ojala, {
-  title: "Ojala",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoRestaurantes
-}).bindPopup("<p>Ojala Bar</p>")
-  .addTo(map);
-
-var cherryPecas = L.marker(cherry, {
-  title: "Cherry Pecas Bar",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoRestaurantes
-}).bindPopup("<p>Cherry Pecas Bar</p>")
-  .addTo(map);
-
-var ovenComida = L.marker(oven, {
-  title: "Oven Restaurante",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoComida
-}).bindPopup("<p>Oven Restaurante</p>")
-  .addTo(map);
-
-var steakBurgers = L.marker(steakBurger, {
-  title: "Steak Burger Restaurante",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoComida
-}).bindPopup("<p>Steak Burger Restaurante</p>")
-  .addTo(map);
-
-var bandoDeEspaña = L.marker(bancoEspaña, {
-  title: "Banco de España",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoBancos
-}).bindPopup("<p>Banco de España</p>")
-  .addTo(map);
-
-var hospital = L.marker(farmacia, {
-  title: "Hospital Gregorio Marañon",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoFarmacias
-}).bindPopup("<p>Hospital Gregorio Marañon</p>")
-  .addTo(map);
-
-var superMercadoCarrefour = L.marker(supermercado, {
-  title: "Carrefour",
-  draggable: false,
-  opacity: 0.95,
-  icon: IconoSuper
-}).bindPopup("<p>Carrefour</p>")
-  .addTo(map);
 
