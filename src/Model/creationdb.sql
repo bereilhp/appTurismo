6ee27a7703ed1e42cc977af627300615686c0029
mariaact <mariactyermo@gmail.com>
2022-10-11 15:35:34 +0200

    insert de la tabla tag

--- src/Model/creationdb.sql
+++ src/Model/creationdb.sql
@@ -10,7 +10,7 @@ CREATE TABLE icon (icon_category VARCHAR(50) NOT NULL, image_icon LONGBLOB NOT N
 
 CREATE TABLE userPlace (email VARCHAR(50) NOT NULL, id_place int NOT NULL AUTO_INCREMENT, PRIMARY KEY (email), PRIMARY KEY (id_place), FOREIGN KEY (email) REFERENCES user, FOREIGN KEY (id_place) REFERENCES place(id_place));
 
-CREATE TABLE tag (id_tag int NOT NULL AUTO_INCREMENT, name VARCHAR(40) NOT NULL, description_tag VARCHAR(100) NOT NULL);
+CREATE TABLE tag (id_tag int NOT NULL AUTO_INCREMENT, name VARCHAR(40) NOT NULL, description_tag VARCHAR(1000) NOT NULL);
 
 CREATE TABLE placeTag (id_place int NOT NULL AUTO_INCREMENT, id_tag int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id_place), PRIMARY KEY (id_tag), FOREIGN KEY (id_place) REFERENCES place(id_place), FOREIGN KEY (id_tag) REFERENCES tag(id_tag));
 
@@ -40,11 +40,28 @@ VALUES
     ("Palacio Vistalegre Arena", "Conciertos", "10:00 - 19:00", "IconoMusica", geomfromtext('point(40.38614, -3.738))')),
     ("Ojala Bar", "Bar", "10:00 - 19:00", "IconoBar", geomfromtext('point(40.42556, -3.70394))')),
     ("Cherry Pecas Bar", "Bar", "10:00 - 19:00", "IconoBar", geomfromtext('point(40.44120, -3.71532))')),
-    ("Oven Restaurante", "Bar", "10:00 - 19:00", "IconoComida", geomfromtext('point(40.42526, -3.70073))')),
-    ("Steak Burger Restaurante", "Bar", "10:00 - 19:00", "IconoComida", geomfromtext('point(40.41990, -3.69937))')),
+    ("Oven Restaurante", "Restaurante", "10:00 - 19:00", "IconoComida", geomfromtext('point(40.42526, -3.70073))')),
+    ("Steak Burger Restaurante", "Restaurante", "10:00 - 19:00", "IconoComida", geomfromtext('point(40.41990, -3.69937))')),
     ("Banco de España", "Bar", "10:00 - 19:00", "IconoBancos", geomfromtext('point(40.41872, -3.69443))')),
-    ("Hospital Gregorio Marañon", "10:00 - 19:00", "IconoFarmacias", geomfromtext('point(40.41966, -3.67130))')),
+    ("Hospital Gregorio Marañon", "Hospital","10:00 - 19:00", "IconoFarmacias", geomfromtext('point(40.41966, -3.67130))')),
     ("Carrefour", "Bar", "10:00 - 19:00", "IconoSuper", geomfromtext('point(40.41793, -3.67321))')),
     ("Universidad CEU San Pablo Monteprincipe", "Bar", "10:00 - 19:00", "IconoMonumento", geomfromtext('point(40.39801, -3.83492))'));
+
+
+INSERT INTO tag (name, description_tag)
+VALUES 
+    ("Museo", "institución sin ánimo de lucro, permanente y al servicio de la sociedad, que investiga, colecciona, conserva, interpreta y exhibe el patrimonio material e inmaterial."),
+    ("Teatro", "El teatro es la rama de las artes escénicas relacionada con la actuación. Representa historias actuadas frente a los espectadores o frente a una cámara usando una combinación de discurso, gestos, escenografía, música, sonido y espectáculo."),
+    ("Jardin", "Un jardín, es una zona del terreno donde se cultivan especies vegetales,  con posible añadidura de otros elementos como fuentes o esculturas, para el placer de los sentidos."),
+    ("Iglesia", "Una iglesia refiere tanto a una comunidad local como a una institución religiosa que agrupa a cristianos de una misma confesión."),
+    ("Monumento", "Construcción arquitectónica o escultórica, generalmente de grandes dimensiones, que se erige en recuerdo de una persona o hecho memorables."),
+    ("Deporte", "Actividad o ejercicio físico, sujeto a determinadas normas, en que se hace prueba, con o sin competición, de habilidad, destreza o fuerza física."),
+    ("Torres", "Construcción de grandes dimensiones fabricada con piedras, ladrillos y materiales resistentes que está destinada a servir de vivienda o de espacio para el desarrollo de una actividad humana."),
+    ("Parque-Atracciones", "Un parque de atracciones o parque de diversiones es un grupo de atracciones de entretenimiento, para montarse y otros acontecimientos en un sitio para el disfrute de un gran número de personas."),
+    ("Centro-Comercial", "Un centro comercial, es una construcción que consta de uno o varios edificios, por lo general de gran tamaño, que albergan servicios, locales y oficinas comerciales aglutinados en un espacio determinado concentrando mayor cantidad de clientes potenciales dentro del recinto."),
+    ("Conciertos", "Un concierto es una expresión cultural musical en que se ejecutan composiciones sueltas."),
+    ("Bar", "Establecimiento en el que hay un mostrador alargado para servir bebidas y algunas comidas."),
+    ("Restaurante", "Establecimiento en el que se preparan y sirven comidas."),
+    ("Hospital", "Establecimiento destinado a proporcionar todo tipo de asistencia médica, incluidas operaciones quirúrgicas y estancia durante la recuperación o tratamiento, y en el que también se practican la investigación y la enseñanza médica.");
 
 
