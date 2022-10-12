36e1e3a9b28bf8cc802ee2fc0b3a1a4c4846e7ef
gemapatricia <gp.herrera@usp.ceu.es>
2022-10-12 20:01:50 +0200

    actualizacion de la bbdd

--- src/Model/creationdb.sql
+++ src/Model/creationdb.sql
@@ -4,9 +4,9 @@ ALTER DATABASE madway CHARACTER SET='utf8' COLLATE='utf8_bin';
 
 CREATE TABLE user (name VARCHAR(30) NOT NULL, surname VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(20) NOT NULL, PRIMARY KEY (email));
 
-CREATE TABLE icon (icon_category VARCHAR(50) NOT NULL, image_icon LONGBLOB NOT NULL, PRIMARY KEY (icon_category));
+CREATE TABLE icon (id_icon INT NOT NULL AUTO_INCREMENT, icon_category VARCHAR(50) NOT NULL, image_icon BLOB, PRIMARY KEY (id_icon));
 
-CREATE TABLE place (id_place INT NOT NULL AUTO_INCREMENT, description_place VARCHAR(200) NOT NULL, schedule VARCHAR(50) NOT NULL, icon_category VARCHAR(50) NOT NULL, coordinate POINT NOT NULL, PRIMARY KEY (id_place), FOREIGN KEY (icon_category) REFERENCES icon(icon_category));
+CREATE TABLE place (id_place INT NOT NULL AUTO_INCREMENT, description_place VARCHAR(200) NOT NULL, schedule VARCHAR(50) NOT NULL, id_icon INT NOT NULL, coordinate POINT NOT NULL, PRIMARY KEY (id_place), FOREIGN KEY (id_icon) REFERENCES icon(id_icon));
 
 CREATE TABLE userPlace (email VARCHAR(50) NOT NULL, id_place INT NOT NULL, PRIMARY KEY (email, id_place), FOREIGN KEY (email) REFERENCES user(email), FOREIGN KEY (id_place) REFERENCES place(id_place));
 
@@ -15,9 +15,10 @@ CREATE TABLE tag (id_tag INT NOT NULL AUTO_INCREMENT, name VARCHAR(40) NOT NULL,
 CREATE TABLE placeTag (id_place INT NOT NULL, id_tag int NOT NULL, PRIMARY KEY (id_tag, id_place), FOREIGN KEY (id_place) REFERENCES place(id_place), FOREIGN KEY (id_tag) REFERENCES tag(id_tag));
 
 
+/*Añadimos manualmente mediante PHPAdmin las imagenes de esta tabla*/
 INSERT INTO icon (icon_category, image_icon)
 VALUES
-    ("IconoMonumento",LOAD_FILE('./Images/IconoMonumento.png')),
+    ("IconoMonumento", LOAD_FILE('./Images/IconoMonumento.png')),
     ("IconoVerde",LOAD_FILE('./Images/IconoVerde.png')),
     ("IconoDeportes",LOAD_FILE('./Images/IconoDeportes.png')),
     ("IconoParqueAtracciones",LOAD_FILE('./Images/IconoParqueAtracciones.png')),
@@ -32,39 +33,74 @@ VALUES
     ("IconoIglesia",LOAD_FILE('./Images/IconoIglesia.png')),
     ("IconoSuper",LOAD_FILE('./Images/IconoSuper.png'));
 
+
     
 
-INSERT INTO place (description_place, schedule, icon_category, coordinate)
+
+
+INSERT INTO place (description_place, schedule, id_icon, coordinate)
 VALUES 
-    ("Museo del prado", "10:00 - 19:00", "IconoMonumento", Point(40.41395, -3.69215)),
-    ("Museo Thyssen", "Museo", "10:00 - 19:00", "IconoMonumento", Point(40.41618, -3.69490)),
-    ("Caixa Forum", "Museo", "10:00 - 20:00", "IconoMonumento", Point(40.41122, -3.69358)),
-    ("Museo Reina Sofia", "Museo", "10:00 - 21:00", "IconoMonumento", Point(40.40805, -3.69460)),
-    ("Teatro Real", "Teatro", "10:00 - 18:30", "IconoMonumento", Point(40.41847, -3.71055)),
-    ("Jardines Sabatini", "Jardin", "9:00 - 22:00", "IconoVerde", Point(40.42048, -3.71405)),
-    ("Palacio Real", "Museo", "10:00 - 18:00", "IconoMonumento", Point(40.41803, -3.71438)),
-    ("Catedral Almudena", "Iglesia", "10:00 - 20:30", "IconoMonumento", Point(40.41596, -3.71455)),
-    ("Parque del Retiro", "Jardin", "18:00 - 21:00", "IconoVerde", Point(40.41317, -3.68307)),
-    ("Puerta de Alcala", "Monumento", "24h", "IconoMonumento", Point(40.42028, -3.68880)),
-    ("Plaza Mayor", "Monumento", "24h", "IconoMonumento", Point(40.41583, -3.70738)),
-    ("Museo Sorolla", "Museo", "9:30 - 20:00", "IconoMonumento", Point(40.43548, -3.69247)),
-    ("Estadio Santiago Bernabeu", "Deporte", "9:30 - 19:00", "IconoVerde", Point(40.45316, -3.68832)),
-    ("Las Cuatro Torres", "Torres", "24h", "IconoMonumento", Point(40.47729, -3.68798)),
-    ("Museo del prado", "Museo", "10:00 - 19:00", "IconMonumento", Point(40.41395, -3.69215)),
-    ("Estadio Civitas Metropolitano", "Deporte", "10:00 - 19:00", "IconoDeportes", Point(40.43633, -3.59948)),
-    ("Parque de atracciones Warner", "Parque-Atracciones", "10:00 - 19:00", "IconoParqueAtracciones", Point(40.44566, -3.65693)),
-    ("Centro comercial ABC Serrano", "Centro-Comercial", "10:00 - 19:00", "IconoWifi", Point(40.43256, -3.68720)),
-    ("Parque de Atracciones de Madrid", "Parque-Atracciones", "10:00 - 19:00", "IconoParqueAtracciones", Point(40.41203, -3.7500)),
-    ("Wizink Center", "Conciertos", "10:00 - 19:00", "IconoMusica", Point(40.42400, -3.67173)),
-    ("Palacio Vistalegre Arena", "Conciertos", "10:00 - 19:00", "IconoMusica", Point(40.38614, -3.738)),
-    ("Ojala Bar", "Bar", "10:00 - 19:00", "IconoBar", Point(40.42556, -3.70394)),
-    ("Cherry Pecas Bar", "Bar", "10:00 - 19:00", "IconoBar", Point(40.44120, -3.71532)),
-    ("Oven Restaurante", "Restaurante", "10:00 - 19:00", "IconoComida", Point(40.42526, -3.70073)),
-    ("Steak Burger Restaurante", "Restaurante", "10:00 - 19:00", "IconoComida", Point(40.41990, -3.69937)),
-    ("Banco de España", "Bar", "10:00 - 19:00", "IconoBancos", Point(40.41872, -3.69443)),
-    ("Hospital Gregorio Marañon", "Hospital","10:00 - 19:00", "IconoFarmacias", Point(40.41966, -3.67130)),
-    ("Carrefour", "Bar", "10:00 - 19:00", "IconoSuper", Point(40.41793, -3.67321)),
-    ("Universidad CEU San Pablo Monteprincipe", "Bar", "10:00 - 19:00", "IconoMonumento", Point(40.39801, -3.83492));
+    ("Museo del prado", "10:00 - 19:00", 1, Point(40.41395, -3.69215)),
+    ("Museo Thyssen", "10:00 - 19:00", 1, Point(40.41618, -3.69490)),
+    ("Caixa Forum",  "10:00 - 20:00", 1, Point(40.41122, -3.69358)),
+    ("Museo Reina Sofia", "10:00 - 21:00", 1, Point(40.40805, -3.69460)),
+    ("Teatro Real", "10:00 - 18:30", 1, Point(40.41847, -3.71055)),
+    ("Jardines Sabatini", "9:00 - 22:00", 2, Point(40.42048, -3.71405)),
+    ("Palacio Real", "10:00 - 18:00", 1, Point(40.41803, -3.71438)),
+    ("Catedral Almudena", "10:00 - 20:30", 1, Point(40.41596, -3.71455)),
+    ("Parque del Retiro", "18:00 - 21:00", 2, Point(40.41317, -3.68307)),
+    ("Puerta de Alcala", "24h", 1, Point(40.42028, -3.68880)),
+    ("Plaza Mayor", "24h", 1, Point(40.41583, -3.70738)),
+    ("Museo Sorolla", "9:30 - 20:00", 1, Point(40.43548, -3.69247)),
+    ("Estadio Santiago Bernabeu", "9:30 - 19:00", 2, Point(40.45316, -3.68832)),
+    ("Las Cuatro Torres", "24h", 1, Point(40.47729, -3.68798)),
+    ("Museo del prado", "10:00 - 19:00", 1, Point(40.41395, -3.69215)),
+    ("Estadio Civitas Metropolitano","10:00 - 19:00", 3, Point(40.43633, -3.59948)),
+    ("Parque de atracciones Warner", "10:00 - 19:00", 4, Point(40.44566, -3.65693)),
+    ("Centro comercial ABC Serrano", "10:00 - 19:00", 5, Point(40.43256, -3.68720)),
+    ("Parque de Atracciones de Madrid",  "10:00 - 19:00", 4, Point(40.41203, -3.7500)),
+    ("Wizink Center",  "10:00 - 19:00", 6, Point(40.42400, -3.67173)),
+    ("Palacio Vistalegre Arena", "10:00 - 19:00", 6, Point(40.38614, -3.738)),
+    ("Ojala Bar", "10:00 - 19:00", 7, Point(40.42556, -3.70394)),
+    ("Cherry Pecas Bar", "10:00 - 19:00", 7, Point(40.44120, -3.71532)),
+    ("Oven Restaurante",  "10:00 - 19:00", 8, Point(40.42526, -3.70073)),
+    ("Steak Burger Restaurante", "10:00 - 19:00", 8, Point(40.41990, -3.69937)),
+    ("Banco de España", "10:00 - 19:00", 10, Point(40.41872, -3.69443)),
+    ("Hospital Gregorio Marañon", "10:00 - 19:00", 9, Point(40.41966, -3.67130)),
+    ("Carrefour", "10:00 - 19:00", 14, Point(40.41793, -3.67321)),
+    ("Universidad CEU San Pablo Monteprincipe", "10:00 - 19:00", 1, Point(40.39801, -3.83492));  
+
+/*INSERT INTO place (description_place, schedule, icon_category, coordinate)
+VALUES 
+    ("Museo del prado", "10:00 - 19:00", 1, Point(40.41395, -3.69215)),
+    ("Museo Thyssen", "10:00 - 19:00", 1, Point(40.41618, -3.69490)),
+    ("Caixa Forum",  "10:00 - 20:00", 1, Point(40.41122, -3.69358)),
+    ("Museo Reina Sofia", "10:00 - 21:00", 1, Point(40.40805, -3.69460)),
+    ("Teatro Real", "10:00 - 18:30", 1, Point(40.41847, -3.71055)),
+    ("Jardines Sabatini", "9:00 - 22:00", 2, Point(40.42048, -3.71405)),
+    ("Palacio Real", "10:00 - 18:00", 1, Point(40.41803, -3.71438)),
+    ("Catedral Almudena", "10:00 - 20:30", 1, Point(40.41596, -3.71455)),
+    ("Parque del Retiro", "18:00 - 21:00", 2, Point(40.41317, -3.68307)),
+    ("Puerta de Alcala", "24h", 1, Point(40.42028, -3.68880)),
+    ("Plaza Mayor", "24h", 1, Point(40.41583, -3.70738)),
+    ("Museo Sorolla", "9:30 - 20:00", 1, Point(40.43548, -3.69247)),
+    ("Estadio Santiago Bernabeu", "9:30 - 19:00", 2, Point(40.45316, -3.68832)),
+    ("Las Cuatro Torres", "24h", 1, Point(40.47729, -3.68798)),
+    ("Museo del prado", "10:00 - 19:00", "IconMonumento", Point(40.41395, -3.69215)),
+    ("Estadio Civitas Metropolitano","10:00 - 19:00", "IconoDeportes", Point(40.43633, -3.59948)),
+    ("Parque de atracciones Warner", "10:00 - 19:00", 4, Point(40.44566, -3.65693)),
+    ("Centro comercial ABC Serrano", "10:00 - 19:00", "IconoWifi", Point(40.43256, -3.68720)),
+    ("Parque de Atracciones de Madrid",  "10:00 - 19:00", 4, Point(40.41203, -3.7500)),
+    ("Wizink Center",  "10:00 - 19:00", 6, Point(40.42400, -3.67173)),
+    ("Palacio Vistalegre Arena", "10:00 - 19:00", 6, Point(40.38614, -3.738)),
+    ("Ojala Bar", "10:00 - 19:00", 7, Point(40.42556, -3.70394)),
+    ("Cherry Pecas Bar", "10:00 - 19:00", 7, Point(40.44120, -3.71532)),
+    ("Oven Restaurante",  "10:00 - 19:00", 8, Point(40.42526, -3.70073)),
+    ("Steak Burger Restaurante", "10:00 - 19:00", 8, Point(40.41990, -3.69937)),
+    ("Banco de España", "10:00 - 19:00", "IconoBancos", Point(40.41872, -3.69443)),
+    ("Hospital Gregorio Marañon", "10:00 - 19:00", "IconoFarmacias", Point(40.41966, -3.67130)),
+    ("Carrefour", "10:00 - 19:00", "IconoSuper", Point(40.41793, -3.67321)),
+    ("Universidad CEU San Pablo Monteprincipe", "10:00 - 19:00", 1, Point(40.39801, -3.83492));*/
 
 
 INSERT INTO tag (name, description_tag)
@@ -82,5 +118,6 @@ VALUES
     ("Bar", "Establecimiento en el que hay un mostrador alargado para servir bebidas y algunas comidas."),
     ("Restaurante", "Establecimiento en el que se preparan y sirven comidas."),
     ("Hospital", "Establecimiento destinado a proporcionar todo tipo de asistencia médica, incluidas operaciones quirúrgicas y estancia durante la recuperación o tratamiento, y en el que también se practican la investigación y la enseñanza médica.");
+
 
 
