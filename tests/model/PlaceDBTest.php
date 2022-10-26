e28d2dbc009ea7ed1b86a88da5187dc16e1c7c2b
PedroB <bereilhp@gmail.com>
2022-10-26 19:02:55 +0200

    Test Separados

--- tests/model/PlaceDBTest.php
+++ tests/model/PlaceDBTest.php
@@ -77,16 +77,32 @@ class PlaceDBTest extends TestCase
         $obj = new App\Model\PlaceDB("madwayTest");
         $obj->insertPlaceData($description_place, $schedule, $id_icon, $latitude, $longitude);
          
-	    $stmt = $obj->getConnection()->prepare("SELECT * FROM place WHERE id_icon = $id_icon");
+	    $stmt = $obj->getConnection()->prepare("SELECT description_place FROM place WHERE id_icon = $id_icon");
 		$stmt->execute();
 
 		$row = $stmt->fetch();
 
-        assertEquals($description_place, $row["description_place"], "Inserted place description does not match the specified one");
+        assertEquals($description_place, $row["description_place"], "Inserted place description does not match the specified one");       
+    }
+
+    public function testInsertPlaceData():void
+    {
+        $description_place = "Museo del prado";
+        $schedule = "10:00 - 19:00";
+        $id_icon = 1;
+        $latitude = 40.41395;
+        $longitude =  -3.69215;
+
+        $obj = new App\Model\PlaceDB("madwayTest");
+        $obj->insertPlaceData($description_place, $schedule, $id_icon, $latitude, $longitude);
+         
+	    $stmt = $obj->getConnection()->prepare("SELECT schedule FROM place WHERE id_icon = $id_icon");
+		$stmt->execute();
+
+		$row = $stmt->fetch();
+
         assertEquals($schedule,          $row["schedule"],          "Inserted place schedule does not match the specified one");
-        assertEquals($id_icon,           $row["id_icon"],           "Inserted place id_icon does not match the specified one");
-        assertEquals($latitude,          $row["latitude"],          "Inserted place latitude does not match the specified one");
-        assertEquals($longitude,         $row["longitude"],         "Inserted place longitude does not match the specified one");       
+               
     }
 
     public function testGetDescriptionPlace():void
