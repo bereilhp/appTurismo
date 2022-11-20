f687acc9bf821cb8ece05366af6ca0ea6d92c18a
Bigosik <62602572+Bigosik@users.noreply.github.com>
2022-11-20 20:33:28 +0100

    Make popups elements
    Add dynamic popups
    Add separate popup.html
    Click handler

--- src/View/map/script.js
+++ src/View/map/script.js
@@ -26,9 +26,9 @@ function createIcon(url){
 Icons = [];
 
 for(var url of locationData.image_icon) {
-  Icons.push(createIcon(url)); 
+  Icons.push(createIcon(url));
 }
- 
+
 //setup map
 //$(document).ready(function () {
 var map = L.map('map').setView([40.416775, -3.703790], 14);
@@ -37,7 +37,7 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-L.geolet({ 
+L.geolet({
   position: 'topleft'
 }).addTo(map);
 
@@ -108,31 +108,46 @@ L.geolet({
 
 L.control.scale().addTo(map);
 
-function drawMarker(data){
+async function drawMarker(data){
   L.marker(data.Coordenadas, {
     title: data.Nombre, //
     draggable: false,
     opacity: 0.95,
     icon: data.Icono
-  }).bindPopup("<div class=\"M" + data.ID  + "\"></div>")
-    .addTo(map).on('click', onClick);
-  $("#M" + data.ID).load("popup.html?name=" + data.Nombre + " .popup")
+  }).bindPopup((layer) => formatPopup(data))
+    .addTo(map);
+
 }
 
-function onClick(e) {
-  alert(this.getLatLng());
+//Function to format a popup (including the click elements)
+function formatPopup(data){
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "popup.html?shit="+ Math.random().toString(), false) //WARNING: "?shit="+ Math.random().toString()" is using to testing popups only!
+  xhr.send();
+  let sel = $('<div>', { id: "M"+ data.ID, html: xhr.responseText});
+  sel.find("div.popup").on('click', () => visibilityChange(sel.find(".popup-big")));
+  sel.find('#name').text(data.Nombre);
+  sel.find('#description').text(data.Description);
+  return sel[0];
 }
 
- 
+function visibilityChange(sel){
+  if (sel.css("display") == "none")
+    sel.css("display","inline");
+  else if (sel.css("display") == "inline")
+    sel.css("display","none");
+}
+
 //draw map markers
 
 for(var i = 0; i < locationData.Locations.length; i++){
   const location = locationData.Locations[i];
-  
+
   drawMarker({
     Coordenadas: [location.latitude, location.longitude],
-    ID: location.ID,
-    Nombre: location.description_place, //hay que cambiar esta linea luego --------------------------------------------------------------------
+    ID: location.id_place,
+    Nombre: location.name_place, //hay que cambiar esta linea luego --------------------------------------------------------------------
+    Description: location.description_place,
     Icono: Icons[location.id_icon - 1] //por array indexing
   });
 }
@@ -158,10 +173,10 @@ const rutas = [];
       });
 
     }if(e.id == 'hacerRuta'){
-      
+
       var origen = document.getElementById('nom1').value;
       var destino = document.getElementById('nom2').value;
-    
+
 
       for(var i = 0; i < locationData.Locations.length; i++){
         const location = locationData.Locations[i];
@@ -170,24 +185,24 @@ const rutas = [];
         }else if(destino.toUpperCase() == location.description_place.toUpperCase()){
           rutas.push(location);
         }else{
-         // console.log("No existe ningún destino con ese nombre");    
+         // console.log("No existe ningún destino con ese nombre");
         }
       }
 
-      var control = L.Routing.control({ 
-        waypoints: [ 
-           L.latLng([rutas[0].latitude, rutas[0].longitude]), 
+      var control = L.Routing.control({
+        waypoints: [
+           L.latLng([rutas[0].latitude, rutas[0].longitude]),
            L.latLng([rutas[1].latitude, rutas[1].longitude])],
            createMarker: function()  {return null},
            router: new L.Routing.osrmv1({
             language: 'en',
            }),
            collapsible: true,
-          
+
         }).addTo(map);
 
         rutas.pop();
-        rutas.pop();   
+        rutas.pop();
     }
   }
 
